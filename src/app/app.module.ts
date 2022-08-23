b065493602daa85a6f66080e208d25c6b97df432
Nestora Zamarripa <maria.martinez5@cox.com>
2022-08-23 11:28:48 -0500

    create new pages, pending add detail

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { DeviceListComponent } from './device-list/device-list.component';
+import { AddDeviceComponent } from './add-device/add-device.component';
+import { DeviceDetailsComponent } from './device-details/device-details.component';
+import { NotificationsComponent } from './notifications/notifications.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    DeviceListComponent,
+    AddDeviceComponent,
+    DeviceDetailsComponent,
+    NotificationsComponent
   ],
   imports: [
     BrowserModule
